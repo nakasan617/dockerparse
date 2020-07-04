7c8f186b32e3b3ff106ae6c3f1b3a7f694496e8d
Yuta Nakamura <nakamura1@cs.uchicago.edu>
2020-07-04 07:33:52 -0500

    parsing

--- parse.py
+++ parse.py
@@ -2,43 +2,19 @@ import os
 import sys
 import shutil
 
-def parse_layers(input_):
-    input_ = input_[input_.find("[") + 1:]
-    input_ = input_[:input_.rfind("]")]
-    shas = input_.split()
-    for i in range(len(shas)):
-        shas[i] = shas[i][shas[i].find(":") + 1:]
-    print(shas)
-        
-    return shas 
-
-def locate_layers(layers):
-    base = "/var/lib/docker/image/overlay2/layerdb/sha256/"
-    for layer in layers:
-        dir_ = base + layer + "/diff"
-        print("one layer")
-        print(dir_)
-        os.system("cat " + dir_)
-
-def locate_layer(layer):
-    base = "/var/lib/docker/image/overlay2/layerdb/sha256/"
-    dir_ = base + layer + "/cache-id"
-    os.system("cat " + dir_)
+def tar_layer(currdir):
+    shutil.copytree(currdir, dest)
     
-def tar_layers(layers, currdir):
-    base = "/var/lib/docker/overlay2/"
+def parse_layers(input_):
     count = 0
-    for layer in layers:
-        #if count > 0:
-        #    break
-        src = base + layer + "/diff/"
-        if os.path.isdir(src):
-            os.system("ls " + src)
-            #shutil.copytree(src, currdir)
-        else:
-            locate_layer(layer)
-
-        #count += 1 
+    index = input_.find(":")
+    while index != -1:
+        portion = input_[:index]
+        input_ = input_[index + 1:]
+        index = input_.find(":")
+        tar_layer(portion)
+    portion = input_
+    tar_layer(portion)
 
 def make_image_dir(imagename):
     os.system("mkdir -p " + imagename)
@@ -54,14 +30,11 @@ def parse_images(filename):
             image = words[1]
             tag = words[2]
             currdir = image + "_" + tag
-            make_image_dir(currdir)
+            make_image_dir(currdir, image)
             order = "docker inspect -f '{{.GraphDriver.Data.LowerDir}}:{{.GraphDriver.Data.UpperDir}}' " + id_
             
             result = os.popen(order).read()
-            #print(image + " " + tag)
-            #layers = parse_layers(result)
-            #tar_layers(layers, currdir)
-            #locate_layer(layers)
+            parse_layers(result)
             count += 1
 
 def pull_images(imagename, wd):
